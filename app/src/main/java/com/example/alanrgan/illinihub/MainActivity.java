7c3b60b1469a558a78de98289133bdc19d90c121
Azriel Alvarado <alvarado_118@yahoo.com>
2018-11-15 00:47:36 -0600

    Integrated MapWrapper and changed from AppCompatActivity (backwards compatibility) to Activity.

--- app/src/main/java/com/example/alanrgan/illinihub/MainActivity.java
+++ app/src/main/java/com/example/alanrgan/illinihub/MainActivity.java
@@ -1,69 +1,60 @@
 package com.example.alanrgan.illinihub;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.mapbox.mapboxsdk.Mapbox;
-import com.mapbox.mapboxsdk.maps.MapView;
-import com.mapbox.mapboxsdk.maps.MapboxMap;
-import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 
-public class MainActivity extends AppCompatActivity {
-    private MapView mapView;
+public class MainActivity extends Activity {
+    private MapWrapper mapWrapper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Mapbox.getInstance(this, getString(R.string.mapbox_access_token));
         setContentView(R.layout.activity_main);
-        mapView = (MapView) findViewById(R.id.mapView);
-        mapView.onCreate(savedInstanceState);
-        mapView.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(MapboxMap mapboxMap) {
-            // Customize map with markers, polylines, etc.
-            }
-        });
+        mapWrapper = new MapWrapper(this, savedInstanceState);
     }
 
     @Override
     public void onStart() {
         super.onStart();
-        mapView.onStart();
+        mapWrapper.onStart();
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        mapView.onResume();
+        mapWrapper.onResume();
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        mapView.onPause();
+        mapWrapper.onPause();
     }
 
     @Override
     public void onStop() {
         super.onStop();
-        mapView.onStop();
+        mapWrapper.onStop();
     }
 
     @Override
     public void onLowMemory() {
         super.onLowMemory();
-        mapView.onLowMemory();
+        mapWrapper.onLowMemory();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        mapView.onDestroy();
+        mapWrapper.onDestroy();
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        mapView.onSaveInstanceState(outState);
+        mapWrapper.onSaveInstanceState(outState);
     }
 }
--- app/src/main/java/com/example/alanrgan/illinihub/MapWrapper.java
+++ app/src/main/java/com/example/alanrgan/illinihub/MapWrapper.java
@@ -1,4 +1,56 @@
 package com.example.alanrgan.illinihub;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+
 public class MapWrapper {
+    private MapView mapView;
+
+    /**
+     * Constructor
+     * @param activity This allows to access Activity members such as findeViewById.
+     * @param savedInstanceState
+     */
+    MapWrapper(Activity activity, Bundle savedInstanceState){
+        mapView = (MapView) activity.findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(MapboxMap mapboxMap) {
+                // Customize map with markers, polylines, etc.
+            }
+        });
+    }
+
+    void onStart() {
+        mapView.onStart();
+    }
+    void onResume() {
+        mapView.onResume();
+    }
+
+    void onPause() {
+        mapView.onPause();
+    }
+
+    void onStop() {
+        mapView.onStop();
+    }
+
+    void onLowMemory() {
+        mapView.onLowMemory();
+    }
+    void onDestroy() {
+        mapView.onDestroy();
+    }
+    void onSaveInstanceState(Bundle outState) {
+        mapView.onSaveInstanceState(outState);
+    }
+
 }
