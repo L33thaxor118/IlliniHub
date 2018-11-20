3b3ff7bfe5ef3ceea21052d8f63428d78da820e9
Azriel Alvarado <alvarado_118@yahoo.com>
2018-11-20 01:09:40 -0600

    Added Database, Event and EventDao. Added an instance of Database to LocationActivity and use it for a test in MainActivity.

---
+++ app/src/main/java/com/example/alanrgan/illinihub/EventDao.java
@@ -0,0 +1,26 @@
+package com.example.alanrgan.illinihub;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+
+@Dao
+public interface EventDao {
+    @Query("SELECT * FROM event")
+    List<Event> getAll();
+
+    @Insert
+    void insertAll(Event... events);
+
+    @Delete
+    void delete(Event event);
+
+    @Query("DELETE FROM event")
+    void deleteAll();
+
+}
+
