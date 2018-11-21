52c5efff081d1f73e2f7b2ad90d3075a8b900a58
Alan Gan <alanrgan@gmail.com>
2018-11-20 22:54:49 -0500

    Added basic example of SlideUp drawer and draggable handle.

--- app/src/main/java/com/example/alanrgan/illinihub/MainActivity.java
+++ app/src/main/java/com/example/alanrgan/illinihub/MainActivity.java
@@ -2,18 +2,26 @@ package com.example.alanrgan.illinihub;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.Gravity;
+import android.view.View;
 
+import com.mancj.slideup.SlideUp;
+import com.mancj.slideup.SlideUpBuilder;
 import com.mapbox.mapboxsdk.annotations.MarkerOptions;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 
 public class MainActivity extends LocationActivity {
   private LocationStore locationStore;
+  private SlideUp slideUp;
+  private View slideView;
 
   @Override
   public void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     locationStore = new LocationStore();
+
+    initializeSlideUp();
   }
 
   @Override
@@ -36,6 +44,19 @@ public class MainActivity extends LocationActivity {
 
     // Start the polling after mapboxMap exists
     locationStore.run();
+  }
+
+  private void initializeSlideUp() {
+    slideView = findViewById(R.id.slideView);
+    slideUp = new SlideUpBuilder(slideView)
+          .withStartState(SlideUp.State.HIDDEN)
+          .withStartGravity(Gravity.BOTTOM)
+          // 'Slide from other view' means that a slide can be triggered
+          // by another view, (e.g. a handle), rather than a button.
+          // We will use a persistent view at the bottom of the map
+          // to let the user drag to show/hide the view.
+          .withSlideFromOtherView(findViewById(R.id.bottomSlideHandle))
+          .build();
   }
 
 }
