5ff515959d0736b44540ba8934db4e5e19ad2564
Alan Gan <alanrgan@gmail.com>
2018-11-26 14:20:06 -0600

    Refactored Event class to use GPSUtils helper

--- app/src/main/java/com/example/alanrgan/illinihub/Event.java
+++ app/src/main/java/com/example/alanrgan/illinihub/Event.java
@@ -3,11 +3,8 @@ import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
+import com.example.alanrgan.illinihub.util.GPSUtils;
 import com.mapbox.mapboxsdk.geometry.LatLng;
-
-import java.util.ArrayList;
-import java.util.List;
-
 
 @Entity
 public class Event {
@@ -30,22 +27,6 @@ public class Event {
   public String tags;
 
   public double distanceFrom(LatLng coord) {
-    double earthRadiusMi = 6371.0 / 1.609344;
-
-    double dLatitude = coord.getLatitude();
-    double dLongitude = coord.getLongitude();
-
-    double diffLatRadians = (latitude - dLatitude) * Math.PI / 180;
-    double diffLonRadians = (longitude - dLongitude) * Math.PI / 180;
-
-    double latRadians = latitude * Math.PI / 180;
-    double dLatRadians = dLatitude * Math.PI / 180;
-
-    double pt = Math.pow(Math.sin(diffLatRadians/2), 2) + Math.pow(Math.sin(diffLonRadians/2), 2)
-        * Math.cos(latRadians) * Math.cos(dLatRadians);
-
-    double distanceMi = 2 * Math.atan2(Math.sqrt(pt), Math.sqrt(1-pt)) * earthRadiusMi;
-
-    return distanceMi;
+    return GPSUtils.milesBetween(new LatLng(latitude, longitude), coord);
   }
 }
